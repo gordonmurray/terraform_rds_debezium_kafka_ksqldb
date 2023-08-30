a14f976f3e9764168c0f4179ab795d9c236fd74d
Gordon Murray <gordon@gordonmurray.com>
2023-08-30 18:25:14 +0100

    update security group to allow apt update

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,6 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-*arm64*"]
   }
 
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
--- security_group_rules_debezium.tf
+++ security_group_rules_debezium.tf
@@ -18,13 +18,12 @@ resource "aws_security_group_rule" "debezium_ssh" {
   description       = "SSH ingress"
 }
 
-resource "aws_security_group_rule" "debezium_registry" {
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
-  cidr_blocks       = ["${var.my_ip_address}/32"]
+resource "aws_security_group_rule" "all_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "all"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.debezium.id
-  description       = "Schema registry UI access"
+  description       = "Allow all out"
 }
-
